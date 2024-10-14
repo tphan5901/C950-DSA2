7f5810a34568059775f6b21853a3f696131e5e88
tphan5901 <37969756+tphan5901@users.noreply.github.com>
2024-10-14 04:11:46 -0700

    Update truck.py

--- truck.py
+++ truck.py
@@ -1,11 +1,5 @@
 #truck class object
 class Truck:
-    # Truck status
-    STATUS_IDLE = 'Idle'
-    STATUS_EN_ROUTE = 'En Route'
-    STATUS_RETURNED = 'Returned'
-    STATUS_MAINTENANCE = 'In Maintenance'
-
     def __init__(self, packages, address, miles, time, truckID):
         self.packages = packages
         self.address = address
@@ -13,12 +7,20 @@ class Truck:
         self.time = time
         self.truckID = truckID
 
+    def __str__(self):
+        #string representation of the Truck object
+        return (f"Truck ID: {self.truckID}, "
+                f"Current Address: {self.address}, "
+                f"Total Miles Driven: {self.miles:.2f}, "
+                f"Total Time: {self.time}, "
+                f"Packages: {', '.join(str(package) for package in self.packages)}")
+        
     #load a package onto the truck
     def load_package(self, package):
         if len(self.packages) < self.capacity:
             self.packages.append(package)
             self.loaded_weight += package.weight
-            package.assign_to_truck(self.truck_id)  # update package status 
+            package.assign_to_truck(self.truck_id) 
         else:
             raise ValueError("Truck capacity reached, cannot load more packages.")
     
